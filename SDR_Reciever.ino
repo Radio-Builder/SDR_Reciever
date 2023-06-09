487ce081a3edaf043be24e27674befc15a756323
irishswguy <irish_guy@hotmail.com>
2023-06-09 15:09:00 +0100

    New cw filter

--- SDR_Reciever.ino
+++ SDR_Reciever.ino
@@ -12,6 +12,14 @@
 #include "FIRConverter.h"
 #include "MixerConverter.h"
 #include "coeff.h"
+#include <EEPROM.h>
+
+
+int eeprom_address = 0;
+#define EEPROM_SIZE 8
+
+
+
 
 //------------------------------- Audio I2S Setup  ------------------------------//
 uint16_t sample_rate=44100;
@@ -167,23 +175,65 @@ void updateTFT()
   tft.drawString(freq,60,30,2);
 }
 
+
+
 void setup()
 {
    Serial.begin(115200);
    while(!Serial);
 
 
+float a_coefficients[]={0.6911109071490495,-12.428960651421141,105.82172676825645,-566.7574909298548,2139.6924386502233,-6046.837515174089,13256.620837359787,-23052.59554787894,32234.12718041634,-36513.630719371824,33586.62601808179,-25027.596550654514,14996.080345346982,-7127.1175654102835,2627.671350537793,-725.1746398136593,141.0702754655541,-17.262293649280707,1};
+float b_coefficients[]={-1,0,9,0,-36,0,84,0,-126,0,126,0,-84,0,36,0,-9,0,1};
+
+float A[]= {
+1.000000000000000000E0,
+-9.375972404244443230E0,
+3.971708140109128320E1,
+-1.001001187522016390E2,
+1.662296925448198510E2,
+-1.900554862369175040E2,
+1.515135948770001790E2,
+-8.316403713801301830E1,
+3.007924141145128070E1,
+-6.473543136946850570E0,
+6.295474410568283830E-1
+};
+
+float B[]= {
+6.652499031261185360E-6,
+0.000000000000000000E0,
+-3.326249515630594010E-5,
+0.000000000000000000E0,
+6.652499031261188910E-5,
+0.000000000000000000E0,
+-6.652499031261188910E-5,
+0.000000000000000000E0,
+3.326249515630594460E-5,
+0.000000000000000000E0,
+-6.652499031261185360E-6
+};
+
+//filtered.setFilter(0, new IIR<float>(B, A));
+//filtered.setFilter(1, new IIR<float>(B, A));
+
+
   
 // Swap coeffs_60plus45 and  coeffs_60minus45 to change sidebands
 
-//  SSB Filter
-//  filtered.setFilter(0, new FIR<float>(SSB_plus45_161_filter));
-//  filtered.setFilter(1, new FIR<float>(SSB_minus45_161_filter));
+  int sw4State = digitalRead(SW_4_PIN);
 
-//  CW Filter
-    filtered.setFilter(0, new FIR<float>(CW_plus45_161_filter));
-    filtered.setFilter(1, new FIR<float>(CW_minus45_161_filter));
-
+  if(sw4State == 0){   
+  //  SSB Filter
+      filtered.setFilter(0, new FIR<float>(SSB_plus45_161_filter));
+      filtered.setFilter(1, new FIR<float>(SSB_minus45_161_filter));
+      }
+  else
+      {
+  //  CW Filter
+      filtered.setFilter(0, new FIR<float>(CW_plus45_261_filter));
+      filtered.setFilter(1, new FIR<float>(CW_minus45_261_filter));
+      }
 //  Filters can be cascaded
 //  filtered.setFilter(1, new FilterChain<float, 2>({new FIR<float>(coeffs_FILTER_1),new FIR<float>(coeffs__FILTER_2)}));
 
@@ -204,10 +254,13 @@ void setup()
   
   in.begin(config);
 
+
+  EEPROM.begin(EEPROM_SIZE);
   Serial.println("I2S started...");
 
   setupDisplay();
   setupEncoder();
+  clk_1_frequency = EEPROM.readLong64(0);
   setupSi5351();
   
   tft.setTextColor(TFT_BLUE,TFT_WHITE );  
@@ -215,13 +268,26 @@ void setup()
   tft.drawString(" OSC-1 ",5,30,2);
 
   updateTFT();
+
+  // EEPROM.begin(EEPROM_SIZE);
+  // size_t write_eeprom = EEPROM.writeULong64(0, clk_1_frequency);
+  // EEPROM.commit();
+  // Serial.println("EEProm Write" + String((uint64_t)write_eeprom));
+
+  uint64_t read_eeprom = EEPROM.readLong64(0);
+  Serial.println("EEProm Read" + String((uint64_t)read_eeprom));
+
+    
 }
 
 void loop()
 { 
-  
+static bool encoderChanged = false;
+
  	if (rotaryEncoder.encoderChanged())
 	{
+    encoderChanged = true;
+
     long currentReading = rotaryEncoder.readEncoder();
     long change =  currentReading -lastEncoderReading;
 
@@ -232,8 +298,24 @@ void loop()
     updateTFT();
     
     SendFrequency();
-	}
+    
+  }
+
 
   copier.copy();
+
+  int sw2State = digitalRead(SW_2_PIN);
+  if(sw2State == 1){  
+    if(encoderChanged !=0){
+      size_t write_eeprom = EEPROM.writeULong64(0, clk_1_frequency);
+      EEPROM.commit();
+      Serial.println("EEProm Write" + String((uint64_t)write_eeprom));
+      encoderChanged = false;
+      }
+  }
+
+  
+
+  
 }
 
